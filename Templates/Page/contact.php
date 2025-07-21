beddd66fa956fdfff5a05ba2875192fa097c356e
Kiryu Setsuna <kiryuset13@gmail.com>
2025-07-22 00:12:54 +0200

    Refonte / modification de la page contact pour améliorer la présentation et les informations

--- Templates/Page/contact.php
+++ Templates/Page/contact.php
@@ -3,33 +3,36 @@
 require_once  BASE_PATH . '/Templates/header.php';
 ?>
 
-<section class="container mt-5 mb-5">
-    <div class="card shadow-sm">
+<!-- main -->
+<section class="container py-5">
+    <div class="form-template">
+        <div class="form-header">
+            <h2>Contactez nous</h2>
+            <p>Renseignez les détails de votre trajet</p>
+        </div>
         <!-- Formulaire pour envoyer le message -->
-        <form method="post" class="card-body contact-form">
-            <!-- Titre -->
-            <h2 class="subtitle-text fs-4 text-center">Contactez nous</h2>
+        <form method="post" class="contact-form">
             <!-- Pseudo et Email-->
-            <div class="d-flex card-body user-info">
+            <div class="form-section">
                 <!-- Pseudo -->
-                <div class="content-text">
+                <div class="form-group content-text">
                     <label for="Pseudo" class="form-labe">Pseudo</label>
                     <input type="text" name="Pseudo" id="Pseudo" class="form-control" placeholder="Joe" required>
                 </div>
                 <!-- Email -->
-                <div class="content-text">
+                <div class="form-group content-text">
                     <label for="email" class="form-labe">Email</label>
                     <input type="email" name="email" id="email" class="form-control" placeholder="joe@gmail.com" required>
                 </div>
             </div>
             <!-- Message -->
-            <div class="content-text">
+            <div class="form-group content-text">
                 <label for="message" class="form-labe">Message</label>
                 <textarea name="message" id="message" class="form-control" placeholder="Votre message" required></textarea>
             </div>
             <!-- Bouton pour envoyer le formulaire -->
-            <div class="d-flex justify-content-center">
-                <button type="submit" class="btn btn-large btn-primary">Envoyer</button>
+            <div class="form-submit">
+                <button type="submit" class="btn btn-filled w-100">Envoyer</button>
             </div>
         </form>
     </div>
